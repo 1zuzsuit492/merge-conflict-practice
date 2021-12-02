d63952064246baabc12d18c3224966d9467d24b5
Younes Tahiri <you@example.com>
2021-12-02 17:08:17 -0500

    Changed var names and added function

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
-if (!c.every((a) => Number.isNumber(a)))
-    throw "All arguments must be numbers.";
-  return c.reduce((a, b) => a + b);
+function getSum(...num) {
+  if (num.every((num) => Number(a))) throw "All arguments must be numbers.";
+  return num.reduce((a, b) => a + b);
 }
 
 fn(1, 2, 3);
